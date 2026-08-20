827f68f4c86ecde4d66c25ba2f2c90d9d6b89ec7
yaaanni <rw.yana.rw@gmail.com>
2026-08-20 14:12:15 +0300

    Initial commit

---
+++ src/test/java/io/github/yaaanni/logix/LogixCoreSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yaaanni.logix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogixCoreSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
